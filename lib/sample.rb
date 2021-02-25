2ce645203248f256e719251dbc9efc3a8ae1e262
YukiYamada <cat.of.attic@gmail.com>
2021-02-25 17:47:53 +0900

    Finish 8(deep_freezable)

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,4 +1,10 @@
-text = "私の誕生日は1997年7月17日です"
-if /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/ =~ text
-  puts "#{year}/#{month}/#{day}"
+class Product
+  SOME_NAME = ['Foo', 'Bar', 'Baz'].freeze
+
+  def self.names_without_foo(names = SOME_NAME)
+    names.delete('Foo')
+  end
 end
+
+Product::SOME_NAME[0].upcase!
+p Product::SOME_NAME
