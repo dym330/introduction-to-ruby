9d24503564e234ea689e5799c23001d03472a775
YukiYamada <cat.of.attic@gmail.com>
2021-02-25 21:33:33 +0900

    finish 8

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,10 +1,11 @@
-class Product
-  SOME_NAME = ['Foo', 'Bar', 'Baz'].freeze
+module Loggable
 
-  def self.names_without_foo(names = SOME_NAME)
-    names.delete('Foo')
-  end
 end
 
-Product::SOME_NAME[0].upcase!
-p Product::SOME_NAME
+class Product
+  include Loggable
+end
+
+p Product.include?(Loggable)
+p Product.included_modules
+p Product.ancestors
